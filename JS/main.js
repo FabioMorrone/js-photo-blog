4ac8bf8e1071e3fbbcdb2e3fe888f5fdbba8aa43
FabioMorrone <morronefabio04@gmail.com>
2025-02-28 16:48:57 +0100

    add

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,25 @@
 const containerEl = document.querySelector('.container')
+const cardEl = document.querySelector('.card')
+
 
 fetch('https://lanciweb.github.io/demo/api/pictures/')
 .then(response => response.json())
 
 .then(data => {
-    console.log(data.response); 
+    console.log(data); 
 })
 
 .catch(error => console.error(error)); 
 
+
+for (let i = 0; i < data.length; i++) {
+    const thisData = data[i];
+    const markup = getDataMarkup(thisData)
+    cardEl.innerHTML += markup
+    console.log(thisData);
+    
+
+
+    
+    
+}
