494d670a86059579f4eb5ba65e4c57f0e66e97a4
FabioMorrone <morronefabio04@gmail.com>
2025-02-28 15:56:46 +0100

    add

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,11 @@
 const containerEl = document.querySelector('.container')
 
+fetch('https://lanciweb.github.io/demo/api/pictures/')
+.then(response => response.json())
+
+.then(data => {
+    console.log(data.response); 
+})
+
+.catch(error => console.error(error)); 
+
