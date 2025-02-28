8931200a3556ebf2c16203245c71173dd04ca009
FabioMorrone <morronefabio04@gmail.com>
2025-02-28 17:45:35 +0100

    add

--- JS/main.js
+++ JS/main.js
@@ -15,33 +15,26 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
             console.log(thisData);
 
         }
-
-
-
-        function getDataMarkup(dataObj) {
-            const { id, title, date, url } = dataObj
-            console.log(id, title, date, url);
-          
-
-            const markup = `
-        <div class="col col-lg-4 ">
-             <div class="card">
-                 <img class="card-img-top" src="./assets/img/CGIC4956.jpeg">
-                    <div class="card-body">
-                         <img class="pin" src="./assets/img/pin.svg" alt="pin">
-                        <p class="card-text m-0">01-07-2024</p>
-                        <h3 class="card-title m-0">SKATE PARK</h3>
-                     </div>
-                </div>
-         </div>`
-            console.log(markup);
-            return markup
-        }
-
-
-
     })
+    function getDataMarkup(dataObj) {
+        const { id, title, date, url } = dataObj
+        console.log(id, title, date, url);
+      
 
+        const markup = `
+    <div class="col mt-3">
+         <div class="card">
+             <img class="card-img-top" src="${url}">
+                <div class="card-body">
+                     <img class="pin" src="./assets/img/pin.svg" alt="pin">
+                    <p class="card-text m-0">${date}</p>
+                    <h3 class="card-title m-0">${title}</h3>
+                 </div>
+            </div>
+     </div>`
+        console.log(markup);
+        return markup
+    }
     
 
 
